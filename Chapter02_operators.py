f3cbd48ada027b90f4ef67af720fb1e78637b121
Sohaib-Aman <96606643+Sohaib-Aman@users.noreply.github.com>
2024-09-02 10:38:27 +0500

    Add files via upload

---
+++ Chapter02_operators.py
@@ -0,0 +1,13 @@
+#python 
+
+print(2+3)    # addition
+print(3-1)    # subtraction
+print(2*3)    # multiplication
+print(7/2)    # divisio operation
+print(7//2)   # floor division operator (give answer in integer form)
+print(7%2)    # modulus operator (return remainder)
+print(2**3)   # power operator
+
+print(3**2/2*3/3+6-4)
+# PEMDAS      #parenthesis exponential multiplication division addition subtraction
+#MD and AS    left to right
---
+++ Chapter_03.py
@@ -0,0 +1,14 @@
+#using single quotes, double quotes and tripple quotes
+
+print('hello')
+print('hello')
+print('''hello''')
+
+#Multiline strings 
+
+#1. using tripple quotes
+x = ''' i am learning python for 
+Artificial intelligence, machine learning, 
+deep learning, data analysis and for data scientist.
+'''
+print(x)
