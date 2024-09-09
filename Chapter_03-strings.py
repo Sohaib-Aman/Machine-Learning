31ab61c136f4571c4c19a614705d8ce89fc17acd
Sohaib-Aman <96606643+Sohaib-Aman@users.noreply.github.com>
2024-09-09 09:10:57 +0500

    Update Chapter_03-strings.py

--- Chapter_03-strings.py
+++ Chapter_03-strings.py
@@ -20,7 +20,25 @@ print(x)
 
 
 #strings are arrays
-
-a="hello,world"
-
+a="hello, world"
 print(a[0])
+
+
+# string slicing 
+#   name[start:end+1]  # range operator
+print(a[0:5])
+print(a[7:12])
+print(a[:5])   # starting index may skip:
+print(a[7:])   # ending index can also be skiped.
+
+# negative indexing
+print(a[-12:-7])
+
+# ----------------- string manipulation ---------------
+
+x = " hello, World "
+print(x.upper()) # string convert into uppercase.
+print(x.lower()) # string convert into lowercase.
+print(x.strip()) # remove spaces from start and end.
+print(x.replace("World", "Python"))
+print(x.split()) # break string and convert into list.
