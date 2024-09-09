4aebc94e236f144d59cc429fbf53bd433ecda042
Sohaib-Aman <96606643+Sohaib-Aman@users.noreply.github.com>
2024-09-09 10:14:23 +0500

    Create chapter06_functions.py

---
+++ chapter06_functions.py
@@ -0,0 +1,20 @@
+#def methodName():
+    # statements
+# simple method not receive and return any value.
+
+def display():
+    print("this is simple method.")    
+display()
+
+# method receiving value which return a value.
+def displayS(a):
+   print(a)
+   
+displayS("python")
+
+# method returning value which return some value.
+def displayR():
+    print("this is returning method")
+    return "bye bye"
+
+print(displayR())
